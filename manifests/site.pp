6ed7fd729b608242fd5bbcd0c9179341f185baff
mrprince42 <princemaryruth@hotmail.com>
2016-10-25 14:11:12 -0500

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,11 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => "I learned the difference between namevar and titles in puppet code.",
+    }
 }
