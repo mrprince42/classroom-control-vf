3e8fd7937eac34416b1b8dc44c8094f0b5fb8d35
mrprince42 <princemaryruth@hotmail.com>
2016-10-27 16:01:57 -0500

    Adding case statements for OS

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,4 +1,36 @@
 class nginx {
+  case$::osfamily {
+    'redhat','debian':{
+    $package   = 'nginx'
+    $owner     = 'root'
+    $group     = 'root'
+    $docroot   = '/var/www'
+    $confdir   = '/etc/nginx/'
+    $logdir    = '/var/log/nginx'
+    $serverdir = '/etc/nginx/conf.d'
+    }
+    
+    'windows':{
+    $package   = 'nginx-service'
+    $owner     = 'Administrator'
+    $group     = 'Administrators'
+    $docroot   = 'C:/ProgramData/nginx/html'
+    $confdir   = 'C:/ProgramData/nginx'
+    $logdir    = 'C:/ProgramData/nginx/logs'
+    $serverdir = 'C:/ProgramData/nginx/conf.d'
+    }
+    
+   default  :{
+     fail("Module ${module_name} is not supported on ${::osfamily}")
+   }
+ }
+ 
+   $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+   }
+
   File {
    owner => 'root',
    group => 'root',
