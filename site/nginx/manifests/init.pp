f7c0f60129d19d3d39cc0e096f417d240b2303b9
mrprince42 <princemaryruth@hotmail.com>
2016-10-27 16:12:19 -0500

    Removing extra nginx in file path

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -52,7 +52,7 @@ class nginx {
    
    file { '${confdir}/nginx.conf':
     ensure  => file,
-    content => epp('nginx.nginx.conf.epp',
+    content => epp('nginx.conf.epp',
       {
         user    => $user,
         confdir => $confdir,
