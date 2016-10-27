f8f59bffe47133d603829684190fa2773b53812d
mrprince42 <princemaryruth@hotmail.com>
2016-10-27 16:16:40 -0500

    Fixing file paths

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -52,7 +52,7 @@ class nginx {
    
    file { '${confdir}/nginx.conf':
     ensure  => file,
-    content => epp('/nginx/nginx.conf.epp',
+    content => epp('/templates/nginx.conf.epp',
       {
         user    => $user,
         confdir => $confdir,
@@ -63,7 +63,7 @@ class nginx {
    
    file { '${confdir}/conf.d/default.conf':
     ensure  => file,
-    content => epp('nginx/default.conf.epp',
+    content => epp('templates/default.conf.epp',
     {
       docroot => $docroot,
     }),
