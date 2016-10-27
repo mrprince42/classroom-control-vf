91bfe3fe3d3decff87fbc79327b9dd06c17be2d0
mrprince42 <princemaryruth@hotmail.com>
2016-10-27 13:29:44 -0500

    Fixing

--- site/aliases/manifests/init.pp
+++ site/aliases/manifests/init.pp
@@ -1,20 +1,19 @@
 class aliases (
     $admin = 'root',
 ) {
-    # uses $admin to build the aliases file
+# uses $admin to build the aliases file
     file { '/etc/aliases':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0644',
-        content => epp('aliases/aliases.epp'),
-            {
-              admin => $admin,
-              }),
-              
-    }
-    exec { '/usr/bin/newaliases':
-        refreshonly => true,
-        subscribe   => File['/etc/aliases'],
+      ensure => file,
+      owner => 'root',
+      group => 'root',
+      mode => '0644',
+      content => epp('aliases/aliases.epp',
+        {
+        admin => $admin,
+        }),
+}
+exec { '/usr/bin/newaliases':   
+    refreshonly => true,
+    subscribe => File['/etc/aliases'],
     }
 }
