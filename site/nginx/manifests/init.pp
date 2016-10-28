fd1d773eb86bbc503180bd074d08d1a782da2da3
mrprince42 <princemaryruth@hotmail.com>
2016-10-28 11:48:08 -0500

    Fixing syntax

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -12,38 +12,39 @@ class nginx (
   File {
     owner => $owner,
     group => $group,
-    mode  => '0664',
+    mode => '0664',
   }
   package { $package:
     ensure => present,
-  }
+   }
   file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
   }
   file { "${docroot}/index.html":
     ensure => file,
     source => 'puppet:///modules/nginx/index.html',
-  } 
+  }
   file { "${confdir}/nginx.conf":
-    ensure  => file,
+    ensure => file,
     content => epp('nginx/nginx.conf.epp',
   {
-      user    => $user,
+      user => $user,
       confdir => $confdir,
-      logdir  => $logdir,
-  }),
+      logdir => $logdir,
+      }),
     notify => Service['nginx'],
   }
   file { "${confdir}/conf.d/default.conf":
-    ensure  => file,
+    ensure => file,
     content => epp('nginx/default.conf.epp',
-    {
-    docroot => $docroot,
-  }),
-    notify  => Service['nginx'],
+  {
+      port => $port,
+      docroot => $docroot,
+      }),
+    notify => Service['nginx'],
   }
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-   }
+   service { 'nginx':
+      ensure => running,
+      enable => true,
+  }
 }
